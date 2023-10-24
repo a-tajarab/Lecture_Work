122f800d1eec44fc58d0669c2164ae2fd3d7ca19
c3667332 <A.Tajarab6656@student.leedsbeckett.ac.uk>
2023-10-24 13:09:19 +0100

    practicing to do loops for last weeks task 3 assignment.

---
+++ 241023.py
@@ -0,0 +1,27 @@
+# a loop model
+num = input("enter a number")
+num = int(num)
+print(num)
+if num > 10:
+    print("your number is too big! INVALID")
+else:
+    print("your number is valid")
+    print("Let's continue")
+
+# a continuous loop for when the end-user has responded incorrectly
+response = ""
+while response != 'y' and response != 'n':
+    response = input("y or n")
+print("you responded with", response)
+
+# printing the message you want backwards
+message = "Hello, World"
+length = len(message)
+myRange = range(length-1, 0, -1)
+for count in myRange:
+    print(message[count], end="")
+
+# iteration
+knights = ["bill", "jack", "ben", "henry"]
+for knight in knights:
+    print(knight)
