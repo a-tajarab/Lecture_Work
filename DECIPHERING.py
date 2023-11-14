b6948dbb624227fcfb4870186b03eb4246556771
c3667332 <A.Tajarab6656@student.leedsbeckett.ac.uk>
2023-11-14 13:16:14 +0000

    using a function to encrypt a message (uses different letters to create the message) and then decrypting the message after.

--- DECIPHERING.py
+++ DECIPHERING.py
@@ -2,11 +2,14 @@ from string import ascii_lowercase as letters
 def rot_13(message):
     rotters = letters[13:] + letters[:13]
     output = ''
-    for c in message:            # iteration
-        output += c
+    for c in message:      # iteration
+        pos = letters.find(c)
+        output += rotters[pos]
     return output
 
 
 if __name__ == '__main__':
     encrypt = rot_13("secret message")
-    print(encrypt)
+    print("encrypted =", encrypt)
+    decrypt = rot_13(encrypt)
+    print("decrypted = ", decrypt)
