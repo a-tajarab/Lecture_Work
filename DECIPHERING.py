b641a492978a73bc61a06c11508b6bbf92efd551
c3667332 <A.Tajarab6656@student.leedsbeckett.ac.uk>
2023-11-14 13:11:05 +0000

    using iteration to reveal a secret message

--- DECIPHERING.py
+++ DECIPHERING.py
@@ -1,7 +1,12 @@
 from string import ascii_lowercase as letters
+def rot_13(message):
+    rotters = letters[13:] + letters[:13]
+    output = ''
+    for c in message:            # iteration
+        output += c
+    return output
 
 
 if __name__ == '__main__':
-    rotters = letters[13:] + letters[:13]
-print(letters)
-print(rotters)
+    encrypt = rot_13("secret message")
+    print(encrypt)
